b2d6b4ac0b5319bb1435b0fc893493f4e0f59733
Bogomol_ML <micklobastov@gmail.com>
2025-05-19 08:26:33 +0300

    beta5

--- logic/searching.py
+++ logic/searching.py
@@ -1,7 +1,6 @@
 # searching.py
 
 from logic.indexer import Indexer
-# from interface.progress_bar import Progress
 from interface.font import Font
 
 
@@ -10,7 +9,6 @@ class Searcher:
         self._request: str = ''
         self._presorted_docs: dict = {}
         self._indexer = Indexer()
-        # self._progress = Progress()
         self._words_dict = self._indexer.inverted_index
 
     def search(self, request):
@@ -30,16 +28,9 @@ class Searcher:
                 return result
 
             result = self._create_result(words, oper)
-            # self._progress.show_progress(total=len(self._presorted_docs))
-            for i, doc_path in enumerate(self._presorted_docs.values(), 1):
-                pass
-                # self._progress.update_progress()
             return result
         except ValueError as err:
             raise err
-        finally:
-            pass
-            # self._progress.close_progress()
 
     def _create_result(self, words, oper):
         docs = set()
@@ -64,6 +55,9 @@ class Searcher:
                     docs.add(el)
         docs = self._sorting_by_relevance(words, list(docs), oper)
 
+        if len(docs) == 0:
+            result = 'Документов не найдено'
+            return result
         result = f'Найдено {Font.GREEN}{len(docs)}{Font.END} документов:\n'
         for i in range(len(docs)):
             for k, v in self._presorted_docs.items():
@@ -77,15 +71,20 @@ class Searcher:
         final_rel: float = 0
         for i in range(len(docs)):
             for j in range(len(words)):
-                relevance = self._indexer.calc_tf_idf(words[j], docs[i])
+                try:
+                    relevance = self._indexer.calc_tf_idf(words[j], docs[i])
+                except KeyError:
+                    continue
                 if oper == 'И' or oper == 'AND':
                     final_rel += relevance
                 elif oper == 'ИЛИ' or oper == 'OR':
                     final_rel = max(final_rel, relevance)
                 else:
                     final_rel = relevance
+                final_rel = round(final_rel, 4)
                 self._presorted_docs[final_rel] = docs[i]
         sorted_rels: list = sorted(self._presorted_docs.keys(), reverse=True)
         for key in sorted_rels:
             sorted_docs.append(self._presorted_docs[key])
+        print(sorted_docs)
         return sorted_docs
